d71a604ad774a35b45a3415ad100bc806d6a6d27
Himanshu Kushwah <himanshu.kushwaha@drivezy.com>
2018-03-02 00:18:23 +0530

    First commit

---
+++ android/src/main/java/com/android/web3rn/Web3RNModule.java
@@ -0,0 +1,46 @@
+package com.android.web3rn;
+
+import android.widget.Toast;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.WalletUtils;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.sample.contracts.generated.Greeter;
+import org.web3j.tx.Contract;
+import org.web3j.tx.ManagedTransaction;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Convert;
+import org.web3j.utils.Numeric;
+
+public class Web3RN extends ReactContextBaseJavaModule {
+
+    public Web3RN(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    public String getName() {
+        return "Web3RN"
+    }
+
+    @ReactMethod
+    public void alert(String message) {
+        Toast.makeText(getReactApplicationContext(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @ReactMethod
+    public void init(String endPoint) {
+        Web3j web3j = Web3j.build(new HttpService(endPoint));
+        return web3j;
+    }
+}
---
+++ android/src/main/java/com/android/web3rn/Web3RNPackage.java
@@ -0,0 +1,33 @@
+package com.android.web3rn;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Web3RNPackage implements ReactPackage {
+
+    @Override
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<NativeModule> createNativeModules(
+        ReactApplicationContext reactContext) {
+        List<NativeModule> modules = new ArrayList<>();
+        modules.add(new Web3RN(reactContext));
+
+        return modules;
+    }
+}
+
